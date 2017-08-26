d719edfd1182c0e5bbcfc0dbe498ff67f432f31a
jorgearagao1 <30787713+jorgearagao1@users.noreply.github.com>
2017-08-26 10:39:28 -0300

    implementada a soma #chupaSoma!

--- src/Janela.java
+++ src/Janela.java
@@ -111,7 +111,30 @@ public class Janela extends javax.swing.JDialog {
     }//GEN-LAST:event_saidaActionPerformed
 
     private void enterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_enterActionPerformed
+       int a=0,b=0,r=0,sun=0; 
         if (this.opcoes.getSelectedIndex() == 0) {    //SOMA
+           
+            for(int i=valor1.getText().length()-1;i>=0;i--){
+            a = Character.getNumericValue(valor1.getText().charAt(i));
+            b = Character.getNumericValue(valor2.getText().charAt(i));
+            if((a+b+r)==2){
+               sun = 0;
+                 r = 1;
+                }
+            if((a+b+r)==3){
+                sun=1;
+                r = 1;
+                }
+            saida.setText(Integer.toString(sun)+saida.getText());
+            }
+            if (r!=0){
+                sun = r+sun;
+                saida.setText(Integer.toString(sun)+saida.getText());
+                }
+            
+            
+            
+            
             System.out.println("SOMA");
         } else {                                      //SUBTRAÇÃO
             System.out.println("SUBTRAÇÃO");
